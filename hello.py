6db1f6184c007184b6d9f2fbdc93f5603f1966ad
Marc <marc.aerne@stud.fhgr.ch>
2024-01-03 16:30:27 +0100

    Flask Server gestartet und index.html für homepage erstellt und verbunden

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("Hello World")
+from flask import Flask, render_template
+
+app = Flask("Mein Semesterprojekt")
+
+#Pfad, den der Benutzer aufruft
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
