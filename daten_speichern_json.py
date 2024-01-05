ade4f4d3309b8f436c219aef5af6a40649bf2888
Marc <marc.aerne@stud.fhgr.ch>
2024-01-05 14:55:30 +0100

    diverses

--- daten_speichern_json.py
+++ daten_speichern_json.py
@@ -1,3 +1,4 @@
+'''
 def save_books(books):
     with open('books.json', 'w') as file:
         json.dump(books, file, indent=4)
@@ -6,3 +7,13 @@ def load_books():
     with open('books.json', 'r') as file:
         books = json.load(file)
     return books
+
+#books einlesen
+books = load_books()
+
+#buch hinzufügen
+books.append({...})
+
+#speichern
+save_books(books)
+'''
