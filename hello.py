75b2f658122b8224e8e1e1541a51e9fd9d6e4f99
Marc <marc.aerne@stud.fhgr.ch>
2024-01-06 11:27:18 +0100

    Anpassungen

--- hello.py
+++ hello.py
@@ -4,8 +4,16 @@ app = Flask("Mein Se")
 
 #Pfad, den der Benutzer aufruft
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    card1 = {
+        'title': 'Karte 1',
+        'content': 'Das ist der Inhalt der ersten Karte'
+    }
+    card2 = {
+        'title': 'Karte 2',
+        'content': 'Das ist der Inhalt der zweiten Karte'
+    }
+    return render_template('index.html', card1=card1, card2=card2)
 
 
 
