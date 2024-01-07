023c5cbc5768fe00edcf74de8d7b9f00edf29dd9
Marc <marc.aerne@stud.fhgr.ch>
2024-01-07 15:58:44 +0100

    Struktur erstellt

--- hello.py
+++ hello.py
@@ -1,20 +1,31 @@
 from flask import Flask, render_template
 
-app = Flask("Mein Se")
+app = Flask("Mein Semesterprojekt")
 
 #Pfad, den der Benutzer aufruft
 @app.route('/')
 def index():
-    card1 = {
-        'title': 'Karte 1',
-        'content': 'Das ist der Inhalt der ersten Karte'
-    }
-    card2 = {
-        'title': 'Karte 2',
-        'content': 'Das ist der Inhalt der zweiten Karte'
-    }
-    return render_template('index.html', card1=card1, card2=card2)
+    return render_template('index.html')
 
+@app.route('/impressum')
+def impressum():
+    return render_template('impressum.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/budget')
+def budget():
+    return render_template('budget.html')
+
+@app.route('/transaktionsverlauf')
+def transaktionsverlauf():
+    return render_template('transaktionsverlauf.html')
+
+@app.route('/berichte')
+def berichte():
+    return render_template('berichte.html')
 
 
 if __name__ == '__main__':
