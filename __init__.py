dabb59263e0127fd0156c816e0cf6f1ba3dedbe6
Marc <marc.aerne@stud.fhgr.ch>
2024-01-17 20:00:09 +0100

    Transaktionsverlauf

--- __init__.py
+++ __init__.py
@@ -36,6 +36,6 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
+    if not path.exists('semesterprojekt/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, session, redirect, url_for
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
@@ -10,39 +10,14 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
-
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
-            db.session.commit()
-            flash('Note added!', category='success')
-
     return render_template("home.html", user=current_user)
 
-
-@views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
-
-'''
 
 transaktions_liste = []
 
 # Pfad, um eine Transaktion hinzuzufügen
-@views.route('/dashboard', methods=['GET', 'POST'])
-def dashboard():
+@views.route('/home', methods=['GET', 'POST'])
+def transaktion_hinzufuegen():
     if request.method == 'POST':
         # Daten aus dem Formular abrufen
         transaktion = request.form['transaktion']
@@ -65,12 +40,12 @@ def dashboard():
         # Die Transaktionsdaten zur Liste hinzufügen
         transaktions_liste.append(transaktion_data)
         
-    return render_template('dashboard.html')
+    return render_template('home.html', user=current_user)
 
 @views.route('/budget')
 def budget():
     budgets = session.get('budgets', [])
-    return render_template('budget.html', budgets=budgets)
+    return render_template('budget.html', budgets=budgets, user=current_user)
 
 
 @views.route('/submit_budget', methods=['POST'])
@@ -87,7 +62,7 @@ def submit_budget():
     session['budgets'].append(new_budget)
     session.modified = True  # Stellt sicher, dass die Session aktualisiert wird
 
-    return redirect(url_for('budget'))
+    return redirect(url_for('budget'), user=current_user)
 
 
 
@@ -95,12 +70,12 @@ def submit_budget():
 
 @views.route('/transaktionsverlauf')
 def transaktionsverlauf():
-    return render_template('transaktionsverlauf.html', transaktionen=transaktions_liste)
+    return render_template('transaktionsverlauf.html', transaktionen=transaktions_liste, user=current_user)
+
 
 
 
 
 @views.route('/berichte')
 def berichte():
-    return render_template('berichte.html')
-'''
+    return render_template('berichte.html', user=current_user)
